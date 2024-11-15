d84f5ad810bc597ab87c8637bbfd61f8927b6e3d
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-11-15 10:54:35 -0600

    Create WQuickUnionUF.java

---
+++ WQuickUnionUF.java
@@ -0,0 +1,40 @@
+
+public class WQuickUnionUF {
+    private int[] id;
+    private int[] size; 
+
+    public WQuickUnionUF(int N) {
+        id = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            size[i] = 1;  
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = root(p);
+        int rootQ = root(q);
+
+        if (rootP == rootQ) return;  
+
+        if (size[rootP] < size[rootQ]) {
+            id[rootP] = rootQ;         
+            size[rootQ] += size[rootP]; 
+        } else {
+            id[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+}
