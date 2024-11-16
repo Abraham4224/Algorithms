73bf20a2cc704532b5b76e2a1f644938c0fb3f98
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-11-16 11:52:59 -0600

    Create WeightedQuickUnionWithPathCompressionUF.java

---
+++ WeightedQuickUnionWithPathCompressionUF.java
@@ -0,0 +1,41 @@
+public class WeightedQuickUnionWithPathCompressionUF {
+    private int[] id;
+    private int[] size;
+
+    public WeightedQuickUnionWithPathCompressionUF(int N) {
+        id = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            id[i] = id[id[i]];  // Path Compression: apunta a su abuelo, aplanando el árbol
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = root(p);
+        int rootQ = root(q);
+
+        if (rootP == rootQ) return;
+
+        // Weighted Union
+        if (size[rootP] < size[rootQ]) {
+            id[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            id[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+}
